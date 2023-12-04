2a2b4820b1a7aa36194fcf071732e59d19a06f1f
Noushedul1 <noushedakib@gmail.com>
2023-12-04 11:38:12 +0600

    category and subcategory show with view composer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Category;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Schema::defaultStringLength(191);
+        view()->composer(['front.includes.navbar'], function ($view) {
+            $view->with('categories',Category::where('status',1)->get());
+        });
     }
 }
